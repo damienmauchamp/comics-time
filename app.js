2e0a30738a5bf943c2fce0f4b01f25d5f612411c
Damien Mauchamp <damienmauchamp@gmail.com>
2019-06-07 14:11:16 +0200

    search request

--- app.js
+++ app.js
@@ -9,18 +9,64 @@ var API = function(config) {
 	this.api_key = config.API_KEY;
 	this.maj_api_key = config.MAJ_API_KEY;
 	this.url = 'comicvine.gamespot.com';
+	this.options = {};
+	this.limit = 50;
+	this.format = 'json';
 }
 
+// utils
+API.prototype.get_prefixe = function (path) {
+	switch(path.replace('/', '')) {
+		case 'issue':
+			return 4000;
+		case 'volume':
+			return 4050;
+		default:
+			return '';
+	}
+	return;
+}
+
+API.prototype.serialize = function (array, first = true) {
+	var str = '';
+	Object.keys(array).forEach(function(key) {
+		str += first ? '?' : '&';
+		first = false;
+		str += (key + "=" + array[key]);
+	});
+	return str;
+}
+
+API.prototype.set_options = function(path, id, query_params) {
+	var prefixe = this.get_prefixe(path);
+	if (!query_params.format) {
+		query_params.format = this.format;
+	}
+	if (!query_params.limit) {
+		query_params.limit = this.limit;
+	}
+
+	this.options = {
+		hostname: this.url,
+		path: '/api/' + path.replace('/', '') + '/'
+			+ ((id && prefixe) ? (prefixe + '-' + id + '/') : '') // {prefixe}-{id}
+			+ '?api_key=' + this.api_key
+			+ this.serialize(query_params, false),
+		headers: {
+			'User-Agent': 'Mozilla/5.0'
+		}
+	};
+	return;
+}
+
+// requests
 API.prototype.get = function (path, params, callback) {
 	return this.request('GET', path, params, callback);
 }
 
 API.prototype.request = function (method, path, params, callback) {
 
-	var self = this
 	var id = null
-	var prefixe = null
-
 	if (typeof params === 'number') {
 		id = params;
 		params = {}
@@ -28,54 +74,17 @@ API.prototype.request = function (method, path, params, callback) {
 		callback = params
 		params = {}
 	}
-
-	// prefixe (XXXX-)
-	switch(path.replace('/', '')) {
-		case 'issue':
-			prefixe = 4000;
-			break;
-		case 'volume':
-			prefixe = 4050;
-			break;
-	}
-
-	params.format = 'json';
-
-	var param = (id && prefixe) ? (prefixe + '-' + id + '/') : '';
-
-	function serialize(array, first = true) {
-		var str = '';
-		Object.keys(array).forEach(function(key) {
-			str += first ? '?' : '&';
-			first = false;
-			str += (key + "=" + array[key]);
-		});
-		return str;
-	}
-
-	var options = {
-		hostname: self.url,
-		path: '/api/' + path.replace('/', '') + '/'
-			+ param
-			+ '?api_key=' + self.api_key
-			+ serialize(params, false),
-		headers: {
-			'User-Agent': 'Mozilla/5.0'
-		}
-	}
+	this.set_options(path, id, params);
 
 	const https = require('https');
-
 	var data = '';
-	https.get(options, function(res) {
+	https.get(this.options, function(res) {
 		res.on('data', function(chunk) {
 			data += chunk;
 		}).on('end', function() {
-
 			var json = JSON.parse(data);
 			callback(json.results);
-
-		})
+		});
 	}).on("error", (err) => {
 		console.log("Error: " + err.message);
 	});
@@ -84,28 +93,36 @@ API.prototype.request = function (method, path, params, callback) {
 }
 
 
-
-
 var api = new API(config);
+
 
 // search
 var search = {
 	query: 'Avengers',
 	resources: 'volume'
-}/*
+}
 api.get('search/', search, function(data) {
+
+	// DISPLAY
+	// {name} {'Volume' || resource_type} {start_year} ({count_of_issues} issues) ({publisher.name})
+	// Avengers Volume 2018 (19 issues) (Marvel)
+
+	// {image->thumb_url}
+	// img : https://static.comicvine.com/uploads/square_mini/6/67663/6406863-01.jpg
 	console.log(data);
 })
+/*
 // issue
 api.get('issue/', 668770, function(data) {
 	console.log(data);
-})*/
+})
+
 // volume
 api.get('volume/', 110496, function(data) {
 	console.log(data);
 })
 
-
+*/
 
 /*
 var express = require("express");
