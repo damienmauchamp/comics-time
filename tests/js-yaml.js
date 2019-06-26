b4f0d24d4cc96d1bffb4483cd90e6b3420340eb7
Damien Mauchamp <damienmauchamp@gmail.com>
2019-06-26 09:05:41 +0200

    extras

--- tests/js-yaml.js
+++ tests/js-yaml.js
@@ -1,34 +1,64 @@
 yaml = require('js-yaml');
 fs = require('fs');
 const filename = './utils/extras.yml'
- 
-// Get document, or throw exception on error
-try {
-	var doc = yaml.safeLoad(fs.readFileSync(filename, 'utf8'));
-	if (typeof doc.extras !== "undefined") {
-		var extras = doc.extras;
 
-		// comics
-		if (typeof extras.comics !== "undefined") {
-			Object.keys(extras.comics).forEach(function(key, i, keys) {
-				var field = extras.comics[key]
+function getExtras(type) {
+	var type_is_null = typeof type === "undefined" || type === ''
+	// Get document, or throw exception on error
+	try {
+		var doc = yaml.safeLoad(fs.readFileSync(filename, 'utf8'));
+		if (typeof doc.extras !== "undefined") {
+			var extras = doc.extras;
 
-				/*var type = field.type
-				var default_value = field.default
-				var nullable = field.nullable
-				var example = field.example*/
+			// comics
+			if (typeof extras.comics !== "undefined" && (type_is_null || type === 'comics')) {
+				console.log('COMICS')
+				_run(extras.comics)
+				//iterate(extras.comics, '')
 
-				//console.log(i, key, field, default_value);
-			})
-			console.log('comics extras: ', extras.comics);
+				/*
+				*/
+				//console.log('comics extras: ', extras.comics);
+			}
+
+			// issues
+			if (typeof extras.issues !== "undefined" && (type_is_null || type === 'issues')) {
+				console.log('ISSUES')
+				_run(extras.issues)
+				//iterate(extras.issues, '')
+				//console.log('issues extras: ', extras.issues);
+			}
+
 		}
-
-		// issues
-		if (typeof extras.issues !== "undefined") {
-			console.log('issues extras: ', extras.issues);
-		}
-
+	} catch (e) {
+		console.log(e);
 	}
-} catch (e) {
-	console.log(e);
 }
+
+function _run(object) {
+	Object.keys(object).forEach(function(key, i, keys) {
+		var field = object[key]
+
+		//var type = field.type
+		//var default_value = field.default
+		//var nullable = field.nullable
+		//var example = field.example
+
+		console.log(i, key, field);
+	})
+}
+
+function iterate(obj, stack) {
+    for (var property in obj) {
+        if (obj.hasOwnProperty(property)) {
+            if (typeof obj[property] == "object") {
+                iterate(obj[property], stack + '.' + property);
+            } else {
+                console.log(property + "   " + obj[property]);
+                //$('#output').append($("<div/>").text(stack + '.' + property))
+            }
+        }
+    }
+}
+
+getExtras('comics')
