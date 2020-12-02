9a2f576687330d3f43ba6e3e139bdb527cde4a50
damienmauchamp <damienmauchamp@gmail.com>
2020-12-02 19:40:38 +0100

    env fix + dev css

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 module.exports = {
 	API_KEY: process.env.API_KEY,
 	MAJ_API_KEY: process.env.MAJ_API_KEY || process.env.API_KEY,
-	ENV: process.env.ENV || '',
+	ENV: process.env.ENVIRONMENT || 'prod',
 	HTTP_PORT: process.env.HTTP_PORT || '9080',
 	HTTPS_PORT: process.env.HTTPS_PORT || '9443',
 	PRIVATE_KEY: process.env.PRIVATE_KEY || '',
