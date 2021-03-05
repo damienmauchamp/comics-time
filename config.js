1594b5c7edb65e290e3d0405c42a7ee77a07bffd
Damien Mauchamp <damienmauchamp@gmail.com>
2021-03-05 16:37:24 +0100

    fixing .env

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const path = require('path');
-//require('dotenv').config({ path: path.resolve(__dirname, './.env') });
-require('dotenv').config();
+require('dotenv').config({ path: path.resolve(__dirname, './.env') });
+// require('dotenv').config();
 
 console.log('process.env', process.env);
 
