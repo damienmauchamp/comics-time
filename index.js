71a8249f12df0d6e89f124cd1ac5f30590555029
damienmauchamp <damienmauchamp@gmail.com>
2022-01-10 02:57:21 +0100

    💄 favicon, icons, logos

--- index.js
+++ index.js
@@ -5,26 +5,26 @@ const config = require('./config.js');
 const ssl = config.PRIVATE_KEY && config.CERTIFICATE;
 
 if (ssl) {
-	var fs = require('fs');
-	var http = require('http');
-	var https = require('https');
-	var privateKey  = fs.readFileSync(config.PRIVATE_KEY, 'utf8');
-	var certificate = fs.readFileSync(config.CERTIFICATE, 'utf8');
-	var credentials = {key: privateKey, cert: certificate};
+  var fs = require('fs');
+  var http = require('http');
+  var https = require('https');
+  var privateKey = fs.readFileSync(config.PRIVATE_KEY, 'utf8');
+  var certificate = fs.readFileSync(config.CERTIFICATE, 'utf8');
+  var credentials = { key: privateKey, cert: certificate };
 }
 
 // Import packages
-const express = require('express')
-const morgan = require('morgan')
+const express = require('express');
+const morgan = require('morgan');
 const favicon = require('serve-favicon');
 
 // App
-const app = express()
+const app = express();
 
 // Morgan
-app.use(morgan('tiny'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(morgan('tiny'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // view engine setup
 const path = require('path');
@@ -35,28 +35,28 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // Routes
-app.use(require('./routes/index.routes'))
+app.use(require('./routes/index.routes'));
 
 // files
-app.use(favicon(__dirname + '/public/favicon.ico'))
+app.use(favicon(__dirname + '/public/favicon.ico'));
 //app.use(favicon(__dirname + '/public/images/logo_64.png'))
 //app.use('/static', express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/public'));
 
 // Starting server
 if (ssl) {
-	var httpServer = http.createServer(app);
-	var httpsServer = https.createServer(credentials, app);
+  var httpServer = http.createServer(app);
+  var httpsServer = https.createServer(credentials, app);
 
-	httpServer.listen(config.HTTP_PORT);
-	httpsServer.listen(config.HTTPS_PORT);
+  httpServer.listen(config.HTTP_PORT);
+  httpsServer.listen(config.HTTPS_PORT);
 
-	console.log('Starting server')
-	console.log(`HTTP on port ${config.HTTP_PORT}`)
-	console.log(`HTTPS on port ${config.HTTPS_PORT}`)
+  console.log('Starting server');
+  console.log(`HTTP on port ${config.HTTP_PORT}`);
+  console.log(`HTTPS on port ${config.HTTPS_PORT}`);
 } else {
-	app.listen(config.HTTP_PORT)
-	console.log(`Starting server on port ${config.HTTP_PORT}`)
+  app.listen(config.HTTP_PORT);
+  console.log(`Starting server on port ${config.HTTP_PORT}`);
 }
 
 //
